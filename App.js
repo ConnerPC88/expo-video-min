307893c9aed1013bf3d910476361cebaa6a7c8c3
Conner Christ <cchrist@sermonaudio.com>
2024-10-01 15:45:37 -0400

    minimum files

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
-import { View, Text } from "react-native";
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useEffect, useRef, useState } from 'react';
+import { PixelRatio, StyleSheet, View, Button } from 'react-native';
 
-export default function App() {
+const videoSource =
+    'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8';
+
+export default function VideoScreen() {
+    const ref = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const player = useVideoPlayer(videoSource, player => {
+        player.loop = true;
+        player.play();
+    });
+
+    useEffect(() => {
+        const subscription = player.addListener('playingChange', isPlaying => {
+            setIsPlaying(isPlaying);
+        });
+
+        return () => {
+            subscription.remove();
+        };
+    }, [player]);
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Universal React with Expo</Text>
+        <View style={styles.contentContainer}>
+            <VideoView
+                ref={ref}
+                style={styles.video}
+                player={player}
+                allowsFullscreen
+                allowsPictureInPicture
+            />
+            <View style={styles.controlsContainer}>
+                <Button
+                    title={isPlaying ? 'Pause' : 'Play'}
+                    onPress={() => {
+                        if (isPlaying) {
+                            player.pause();
+                        } else {
+                            player.play();
+                        }
+                        setIsPlaying(!isPlaying);
+                    }}
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 50,
+    },
+    video: {
+        width: 350,
+        height: 275,
+    },
+    controlsContainer: {
+        padding: 10,
+    },
+});
